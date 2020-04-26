2d67b4fd75df6a42124934f982203a708bd902c3
Adnan Hussain Turki <adnanhussainturki@gmail.com>
2020-04-27 04:29:54 +0530

    Endurance user directory

--- misc/endurance_user/install.sh
+++ misc/endurance_user/install.sh
@@ -2,10 +2,12 @@ clear
 echo "***************************************";
 echo "*        Building Endurance User      *"
 echo "***************************************";
-
+mkdir /home/endurance
+chmod 711 /home/endurance
 useradd -m endurance --home /home/endurance
 echo "Enter the password for endurance:"
 passwd endurance
+chown endurance.endurance endurance
 
 echo "***************************************";
 echo "*         Endurance User Built        *"
