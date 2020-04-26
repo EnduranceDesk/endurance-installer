0bdb5811c0f765e21014897eb48aef45cbbefaf9
Adnan Hussain Turki <adnanhussainturki@gmail.com>
2020-04-27 04:56:10 +0530

    Chown fix.

--- misc/endurance_user/install.sh
+++ misc/endurance_user/install.sh
@@ -7,7 +7,7 @@ chmod 711 /home/endurance
 useradd -m endurance --home /home/endurance
 echo "Enter the password for endurance:"
 passwd endurance
-chown endurance.endurance endurance
+chown endurance.endurance /home/endurance
 
 echo "***************************************";
 echo "*         Endurance User Built        *"
