3532b8114c2fa969d2323f666c2fa8ac08953e6c
Adnan Hussain Turki <adnanhussainturki@gmail.com>
2020-04-28 02:43:32 +0530

    Fix path

--- endeavour/install.sh
+++ endeavour/install.sh
@@ -2,15 +2,15 @@ echo "***************************************";
 echo "*        INSTALLING ENDEAVOUR         *"
 echo "***************************************"
 
-mkdir -p /etc/repo
-cd /etc/repo
+mkdir -p /etc/endurance/repo
+cd /etc/endurance/repo
 git clone https://github.com/AdnanHussainTurki/endeavour
 
 
-mkdir -p /etc/current
+mkdir -p /etc/endurance/current
 cp -rf /etc/repo/endeavour /etc/current/endeavour
 
-cd /etc/current/endeavour
+cd /etc/endurance/current/endeavour
 sh deploy.sh
 
 
