fcf955e87be4000c4242a67c277ed6c1a9c8bdc3
Adnan Hussain Turki <adnanhussainturki@gmail.com>
2020-04-28 03:17:54 +0530

    Exit when password failed

--- misc/endurance_user/install.sh
+++ misc/endurance_user/install.sh
@@ -7,6 +7,11 @@ chmod 711 /home/endurance
 useradd -m endurance --home /home/endurance
 echo "Enter the password for endurance:"
 passwd endurance
+if [ $? -eq 0 ]; then
+    echo "Password for endurance successfully set."
+else
+    exit 1
+fi
 chown endurance.endurance /home/endurance
 
 echo "***************************************";
