3f8331b1b06fc0dad3d8be1ba4f4bbe50173418c
Adnan <adnanhussainturki@gmail.com>
2020-05-31 19:30:56 +0530

    PHP FPM

--- mysql/install.sh
+++ mysql/install.sh
@@ -1,7 +1,7 @@
 # @Author: Adnan
 # @Date:   2020-04-21 14:32:38
 # @Last Modified by:   Adnan
-# @Last Modified time: 2020-05-28 12:55:53
+# @Last Modified time: 2020-05-31 12:07:46
 clear
 echo "***************************************";
 echo "*           MySQL Installing           *"
@@ -21,10 +21,13 @@ echo "Enter root MySQL password:"
 mysql -u root -p << EOF
 CREATE DATABASE endurance;
 CREATE USER 'endurance_user'@'%' IDENTIFIED WITH mysql_native_password BY 'endurancekapassword';
+EOF
+
+echo "Enter root MySql password again:"
+mysql -u root -p << EOF
 GRANT ALL PRIVILEGES ON `endurance`.* TO 'endurance_user'@'%';
 ALTER USER 'endurance_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 EOF
-
 
 echo "***************************************";
 echo "*           MySQL Installed           *"
