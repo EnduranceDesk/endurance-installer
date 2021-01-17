39a51d09a688b45ab8b533c779acc2caa85f6d83
Adnan Hussain Turki <adnanhussainturki@gmail.com>
2021-01-18 00:18:49 +0530

    Endeavour IP

--- misc/endurance_user/install.sh
+++ misc/endurance_user/install.sh
@@ -7,6 +7,10 @@ echo "***************************************";
 chmod 711 /home/endurance
 
 
+ENDEAVOURIP=$(curl -s http://whatismyip.akamai.com/)
+echo -e $ENDEAVOURIP  | tr -d '\n' > /home/endurance/endeavour.ip
+chown endurance.endurance /home/endurance/endeavour.ip
+
 echo "***************************************";
 echo "*         Endurance User Built        *"
 echo "***************************************";
--- misc/rover_user/install.sh
+++ misc/rover_user/install.sh
@@ -6,6 +6,9 @@ echo "***************************************";
 /usr/sbin/useradd rover -d /home/rover  -m  -r
 chmod 711 /home/rover
 
+ENDEAVOURIP=$(curl -s http://whatismyip.akamai.com/)
+echo -e $ENDEAVOURIP  | tr -d '\n' > /home/rover/endeavour.ip
+chown rover.rover /home/rover/endeavour.ip
 
 echo "***************************************";
 echo "*         Endurance Rover Built        *"
