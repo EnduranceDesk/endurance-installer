87a57b1f27d35b9e1923b27ef80aacc18aec21cd
Adnan <adnanhussainturki@gmail.com>
2022-03-01 15:15:36 +0530

    Set

--- base_install.sh
+++ base_install.sh
@@ -12,8 +12,11 @@ SELINUX=disabled
 SELINUXTYPE=targeted
 EOF
 
+chattr -V -i /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+chattr -V +i /etc/resolv.conf
+
 
 bash "$PWD/misc/package_updater/install.sh"
 bash "$PWD/wget/install.sh"
